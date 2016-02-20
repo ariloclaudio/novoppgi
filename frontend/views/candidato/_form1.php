63216a42a2b4c4ef59852a53b33c24bf915657db
Thiago Leite Takahashi <thiagoleitexd@gmail.com>
2016-02-20 15:26:02 -0200

    validacoes do form1

--- frontend/views/candidato/_form1.php
+++ frontend/views/candidato/_form1.php
@@ -17,28 +17,36 @@ $estadoscivil = ['Solteiro(a)' => 'Solteiro(a)', 'Casado(a)' => 'Casado(a)', 'Di
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
+<script>
+
+
+
+</script>
+
 <div class="candidato-form">
 
     <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']]); ?>
 
 <!-- Inicio da Identificação do Candidato -->
-
+    
     <div style="width: 100%; clear: both;"><p align="justify"><b>Identificação do Candidato</b></p></div>
-
+    <div class ="row">
     <?= $form->field($model, 'nome', ['options' => ['class' => 'col-md-6']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Nome:</b>") ?>
 
     <?= $form->field($model, 'estadocivil', ['options' => ['class' => 'col-md-3']])->dropDownList($estadoscivil, ['prompt' => 'Selecione um estado Civil..'])->label("<font color='#FF0000'>*</font> <b>Estado Civil:</b>") ?>
 
     <?= $form->field($model, 'sexo', ['options' => ['class' => 'col-md-3']])->radioList(['M' => 'Masculino', 'F' => 'Feminino'], ['style' => 'height: 34px;'])->label("<font color='#FF0000'>*</font> <b>Sexo:</b>") ?>
-
+    </div>
     <div class="col-md-12">
+    <div class ="row">
         <?= $form->field($model, 'cep', ['options' => ['class' => 'col-md-4', 'style' => 'padding-left: 0px;']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>CEP:</b>") ?>
 
         <?= $form->field($model, 'uf', ['options' => ['class' => 'col-md-4']])->dropDownList($ufs, ['prompt' => 'Selecione UF..'])->label("<font color='#FF0000'>*</font> <b>Estado:</b>") ?>
 
         <?= $form->field($model, 'cidade', ['options' => ['class' => 'col-md-4']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Cidade:</b>") ?>
     </div>
-
+    </div>
+    <div class ="row">
     <?= $form->field($model, 'endereco', ['options' => ['class' => 'col-md-6']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Endereço:</b>") ?>
 
     <?= $form->field($model, 'bairro', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Bairro:</b>") ?>
@@ -50,7 +58,8 @@ $estadoscivil = ['Solteiro(a)' => 'Solteiro(a)', 'Casado(a)' => 'Casado(a)', 'Di
         ]
     ])->label("<font color='#FF0000'>*</font> <b>Data de Nascimento:</b>")
     ?>
-
+    </div>
+    <div class ="row">
     <?= $form->field($model, 'nacionalidade', ['options' => ['class' => 'col-md-12']])->radioList(['1' => 'Brasileira', '2' => 'Estrangeira'])->label("<font color='#FF0000'>*</font> <b>Nacionalidade:</b>") ?>
 
     <div id="divEstrangeiro" style='display: none;'>
@@ -60,12 +69,16 @@ $estadoscivil = ['Solteiro(a)' => 'Solteiro(a)', 'Casado(a)' => 'Casado(a)', 'Di
 
         <?= $form->field($model, 'passaporte', ['options' => ['class' => 'col-md-6']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Passaporte:</b>") ?>
     </div>
-
+    </div>
     <div id="divBrasileiro" style="display: none;">
+    <div class="row">
         <p align="justify" class="col-md-12"><b>Estes campos são obrigatórios para candidatos com nacionalidade Brasileira</b></p>
-
-        <?= $form->field($model, 'cpf', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>CPF:</b>") ?>   
-
+        <?= $form->field($model, 'cpf', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <div style='display:inline;' id = 'corCPF'><b>CPF:</b> </div>") ?>   
+    </div>
+    <div class="row" style="margin-left:0px; margin-bottom:10px; ">
+            <div id = "errocpf" style="color:#a94442; display:none;"> CPF é campo obrigatório para brasileiros </div>
+    </div>
+    <div class="row">
         <?= $form->field($model, 'rg', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>RG:</b>") ?>
 
         <?= $form->field($model, 'orgaoexpedidor', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Orgão Expedidor:</b>") ?>
@@ -77,47 +90,54 @@ $estadoscivil = ['Solteiro(a)' => 'Solteiro(a)', 'Casado(a)' => 'Casado(a)', 'Di
             ]
         ])->label("<font color='#FF0000'>*</font> <b>Data de Expedição:</b>")
     ?>
+    </div>  
     </div>
 
 <!-- Fim da Identificação do Candidato -->
-    
-    <div style="width: 100%; clear: both;"><p align="justify"><b>Telefones de Contato</b></p></div>
 
+    <div style="width: 100%; clear: both;"><p align="justify"><b>Telefones de Contato</b></p></div>
+    <div class = "row">
     <?= $form->field($model, 'telresidencial', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Telefone Residencial:</b>") ?>
 
     <?= $form->field($model, 'telcomercial', ['options' => ['class' => 'col-md-3', 'style' => 'margin-left: 50px;']])->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'telcelular', ['options' => ['class' => 'col-md-3', 'style' => 'margin-left: 50px;']])->textInput(['maxlength' => true]) ?>
+    </div>
 
     <div style="width: 100%; clear: both;"><p align="justify"><b>Filiação</b></p></div>
-
+    <div class = "row">
     <?= $form->field($model, 'nomepai', ['options' => ['class' => 'col-md-6']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Nome do Pai:</b>") ?>
 
     <?= $form->field($model, 'nomemae', ['options' => ['class' => 'col-md-6']])->textInput(['maxlength' => true])->label("<font color='#FF0000'>*</font> <b>Nome da Mãe:</b>") ?>
+    </div>
 
     <div style="width: 100%; clear: both;"><p align="justify"><b>Dados do PosComp</b></p></div>
-
+    <div class = "row">
     <?= $form->field($model, 'inscricaoposcomp', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'anoposcomp', ['options' => ['class' => 'col-md-3']])->textInput() ?>
 
     <?= $form->field($model, 'notaposcomp', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true]) ?>
+    </div>
 
     <div style="width: 100%; clear: both;"><p align="justify"><b>Dados da Inscrição</b></p></div>
-
+    <div class = "row">
     <?= $form->field($model, 'cursodesejado', ['options' => ['class' => 'col-md-6']])->radioList(['1' => 'Mestrado' , '2' => 'Doutorado'])->label("<font color='#FF0000'>*</font> <b>Curso Desejado:</b>")  ?>
 
     <?= $form->field($model, 'regime', ['options' => ['class' => 'col-md-6']])->radioList(['1' => 'Integral', '2' => 'Parcial'])->label("<font color='#FF0000'>*</font> <b>Regime de Dedicação:</b>") ?>
-
+    </div>
+    <div class = "row">
     <?= $form->field($model, 'vinculoconvenio', ['options' => ['class' => 'col-md-6']])->radioList(['SIM' => 'Sim', 'NAO' => 'Não'])->label("<font color='#FF0000'>*</font> <b>Vinculado a algum Convênio?</b>")  ?>
 
     <?= $form->field($model, 'convenio', ['options' => ['class' => 'col-md-6']])->textInput(['maxlength' => true])->label("<b>Quais (ex: PICTD)?</b>") ?>
-
+    </div>
+        <div class = "row">
     <?= $form->field($model, 'solicitabolsa', ['options' => ['class' => 'col-md-6']])->radioList(['SIM' => 'Sim', 'NAO' => 'Não'])->label("<font color='#FF0000'>*</font> <b>Solicita Bolsa de Estudo?</b>")  ?>
 
     
     <?= $form->field($model, 'vinculoemprego', ['options' => ['class' => 'col-md-6']])->radioList(['SIM' => 'Sim', 'NAO' => 'Não'])->label("<font color='#FF0000'>*</font> <b>Manterá Vínculo Empregatício?</b>") ?>
-
+    </div>
+    </div>
     <div id="divVinculo">
     <p align="justify" class="col-md-12"><b>Estes campos n&#227;o s&#227;o obrigat&#243;rios </b> (Se desejado, anexe o arquivo contendo a carta do empregador comprometendo-se a limitar a carga de trabalho do candidato a 24 horas semanais, ou meio expediente de trabalho)</p>
 
@@ -130,9 +150,12 @@ $estadoscivil = ['Solteiro(a)' => 'Solteiro(a)', 'Casado(a)' => 'Casado(a)', 'Di
     
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Salvar e Continuar' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+
+        <?= Html::submitButton($model->isNewRecord ? 'Salvar e Continuar' : 'Update', ['onclick' => 'validacao()','class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
 
 </div>
+
--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -37,4 +37,56 @@ $(document).ready( function() {
    			$('#divBrasileiro').css('display', 'none');
    		}
    });
+
+   function verificacpf(cpf){
+      if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
+        return false;
+     add = 0;
+
+     for (i=0; i < 9; i ++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+
+     rev = 11 - (add % 11);
+     if (rev == 10 || rev == 11)
+        rev = 0;
+
+     if (rev != parseInt(cpf.charAt(9)))
+        return false;
+
+     add = 0;
+     for (i = 0; i < 10; i ++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+
+     rev = 11 - (add % 11);
+     if (rev == 10 || rev == 11)
+         rev = 0;
+
+     if (rev != parseInt(cpf.charAt(10)))
+         return false;
+
+     return true;
+   }
+
+
+  $('#candidato-cpf').blur(function(){
+
+    var cpf = $('#candidato-cpf').val();
+    cpf = verificacpf(cpf);
+    if (cpf == false){
+      //alert("falso");
+    }
+    else{
+      //alert("true");
+    }
+
+  });
+
 });
+
+
+
+function validacao(){
+  $('#errocpf').css('display', 'block');
+  $('#candidato-cpf').css('border-color', '#a94442');
+  $('#corCPF').css('color', '#a94442');
+}
