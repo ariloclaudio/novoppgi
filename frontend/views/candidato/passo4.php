5e0686288665b678dafbf3461cc06816ee1a00e4
Thiago Leite Takahashi <thiagoleitexd@gmail.com>
2016-03-07 00:18:43 -0300

    passo 4, alteracoes

--- frontend/views/candidato/passo4.php
+++ frontend/views/candidato/passo4.php
@@ -43,31 +43,32 @@ $this->params['breadcrumbs'][] = $this->title;
 
 	<div class="login-box" style="width:100%;">
 	    <div class="login-logo">
-	        <p align="center"><h3> Inscrição realizada com sucesso !!!</h3></p>
+	        <p><h3 style="text-align:center;"> Inscrição realizada com sucesso !</h3></p>
 	    </div>
 		<div style="width: 100%; border:solid 0px">
 
-				   	<p><font size="2" style="line-height: 150%"><br>Seus dados foram cadastrados com sucesso. Sua proposta será analisada e em breve teremos a divulgação dos aprovados no processo de seleção do PPGI.<br /> Clique no link "Imprimir Formulário de Inscrição" e imprima a página contendo os dados de sua incrição para seu controle. Quando solicitado, assine e entregue este formulário na secretaria do PPGI/UFAM que fica no Departamento de Ciência da Computação, localizado na Rua Gen. Rodrigo Octávio Jordão Ramos, 3000 SETOR NORTE do Campus Universitário Manaus - AM - CEP 69.077-000.</font></p>
+				   	<p><font size="2" style="text-align:left;"><br>Seus dados foram cadastrados com sucesso. Sua proposta será analisada e em breve teremos a divulgação dos aprovados no processo de seleção do PPGI.<br /> Clique no link "Imprimir Formulário de Inscrição" e imprima a página contendo os dados de sua incrição para seu controle. Quando solicitado, assine e entregue este formulário na secretaria do PPGI/UFAM que fica no Departamento de Ciência da Computação, localizado na Rua Gen. Rodrigo Octávio Jordão Ramos, 3000 SETOR NORTE do Campus Universitário Manaus - AM - CEP 69.077-000.</font></p>
 
 				   	<br /><p align="right"><font size="2" style="line-height: 150%">Ass: Coordenação do PPGI/UFAM</font></p><br />
 		</div>
 	</div>
-	   		<div style="text-align: center;">
+			<div style="text-align:center; font-weight: bold; margin-left: 0%;margin-bottom: 2%; margin-top: 2%"> Clique nos ícones abaixo para visualizar os respectivos documentos em formato pdf. </div>
+	   		<div style="margin-left: 8%; margin-right:10%; text-align:center; width:80%;">
 		   		<a target="resource window" href="index.php?r=candidato/comprovanteinscricao">
-					<div class="iconimage" style="float: left; width: 200px; margin-top: 20px;"><img src="img/edit_f2.png" border="0" height="32" width="32"><br><b>Imprimir Formul&#225;rio de Inscri&#231;&#227;o</b></div></a>
+					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/edit_f2.png" border="0" height="32" width="32"><br><b>Imprimir Formul&#225;rio de Inscri&#231;&#227;o</b></div></a>
 
 				<a target="resource window" href="<?php echo $diretorio;?>Proposta.pdf">
-					<div class="iconimage" style="float: left; width: 200px; margin-top: 20px;"><img src="img/edit_f2.png" border="0" height="32" width="32"><br><b>Imprimir Proposta de Trabalho</b></div></a>
+					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/edit_f2.png" border="0" height="32" width="32"><br><b>Imprimir Proposta de Trabalho</b></div></a>
 		        <div id="cpanel" width="25%"></div>
 				
 				<a target="resource window" href="<?php echo $diretorio;?>Historico.pdf">
-						<div class="iconimage" style="float: left; width: 200px; margin-top: 20px;"><img src="img/historico.gif" border="0" height="32" width="32"><br><b>Imprimir Hist&#243;rico Escolar</b></div></a>
+						<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/historico.gif" border="0" height="32" width="32"><br><b>Imprimir Hist&#243;rico Escolar</b></div></a>
 				<a target="resource window" href="<?php echo $diretorio;?>Curriculum.pdf">
-						<div class="iconimage" style="float: left; width: 200px; margin-top: 20px;"><img src="img/curriculum.gif" border="0" ><br><b>Imprimir Curriculum Lattes</b></div></a>
+						<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/curriculum.gif" border="0" ><br><b>Imprimir Curriculum Lattes</b></div></a>
 				<a target="resource window" href="<?php echo $diretorio;?>Comprovante.pdf">
-						<div class="iconimage" style="float: left; width: 200px; margin-top: 20px;"><img src="img/comprovante.png" border="0" height="32" width="32"><br><b>Imprimir Comprovante de Pagamento</b></div></a>
+						<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/comprovante.png" border="0" height="32" width="32"><br><b>Imprimir Comprovante de Pagamento</b></div></a>
 				<a target="resource window" href="<?php echo $diretorio;?>cartaempregador.pdf">
-					<div class="iconimage" style="float: left; width: 200px; margin-top: 20px;"><img src="img/carta.jpg" border="0" height="32" width="32"><br><b>Imprimir Carta do Empregador</b></div></a>
+					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/carta.jpg" border="0" height="32" width="32"><br><b>Imprimir Carta do Empregador</b></div></a>
 			</div>
 
 </div>
