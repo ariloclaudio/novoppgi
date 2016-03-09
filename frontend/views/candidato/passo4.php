5982e36f94bcbe554f14924af6c58413fe6d23a4
Thiago Leite Takahashi <thiagoleitexd@gmail.com>
2016-03-09 17:51:38 -0400

    alteracoes gerais

--- frontend/views/candidato/passo4.php
+++ frontend/views/candidato/passo4.php
@@ -50,12 +50,12 @@ $this->params['breadcrumbs'][] = $this->title;
 		</div>
 	</div>
 			<div style="text-align:center; font-weight: bold; margin-left: 0%;margin-bottom: 2%; margin-top: 2%"> Clique nos ícones abaixo para visualizar os respectivos documentos em formato pdf. </div>
-	   		<div style="margin-left: 8%; margin-right:10%; text-align:center; width:80%;">
+	   		<div style="margin-left: 16%; margin-right:10%; text-align:center; width:80%;">
 		   		<a target="resource window" href="index.php?r=candidato/comprovanteinscricao">
 					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/edit_f2.png" border="0" height="32" width="32"><br><b>Imprimir Formul&#225;rio de Inscri&#231;&#227;o</b></div></a>
 
 				<a target="resource window" href="<?php echo $diretorio;?>Proposta.pdf">
-					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/edit_f2.png" border="0" height="32" width="32"><br><b>Imprimir Proposta de Trabalho</b></div></a>
+					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/trabalho.png" border="0" height="32" width="32"><br><b>Imprimir Proposta de Trabalho</b></div></a>
 		        <div id="cpanel" width="25%"></div>
 				
 				<a target="resource window" href="<?php echo $diretorio;?>Historico.pdf">
@@ -63,9 +63,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				<a target="resource window" href="<?php echo $diretorio;?>Curriculum.pdf">
 						<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/curriculum.gif" border="0" ><br><b>Imprimir Curriculum Lattes</b></div></a>
 				<a target="resource window" href="<?php echo $diretorio;?>Comprovante.pdf">
-						<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/comprovante.png" border="0" height="32" width="32"><br><b>Imprimir Comprovante de Pagamento</b></div></a>
-				<a target="resource window" href="<?php echo $diretorio;?>cartaempregador.pdf">
-					<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/carta.jpg" border="0" height="32" width="32"><br><b>Imprimir Carta do Empregador</b></div></a>
+						<div class="iconimage" style="float: left; width: 150px; margin-top: 20px;"><img src="img/pagamento.png" border="0" height="32" width="32"><br><b>Imprimir Comprovante de Pagamento</b></div></a>
 			</div>
 
 </div>
