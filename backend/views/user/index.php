01a5152d487c0b9bb05400a8d451f7aa53242913
Fabricio Lima <fabriciolima31@gmail.com>
2016-03-25 17:50:45 -0400

    Adição de arquivos faltantes

---
+++ backend/views/user/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Usuários';
+?>
+<div class="user-index">
+
+    <p>
+        <?= Html::a('<span class="fa fa-plus"></span> Adicionar Usuário', ['site/signup'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        //'filterModel' => $searchModel,
+        'columns' => [
+            'nome',
+            'username',
+             'email:email',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
--- frontend/views/recomendacoes/cartarecomendacaomsg.php
+++ frontend/views/recomendacoes/cartarecomendacaomsg.php
@@ -9,12 +9,11 @@ use yii\helpers\Html;
 $this->title ='Carta de Recomendacão Mestrado/Doutorado no PPGI/UFAM';
 ?>
 <div class="recomendacoes-create">
-
+	<hr align='tr'>
     <div class="alert alert-success" role="alert">
 	  <span class="sr-only">Error:</span>
 	<h4><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Carta de Recomendação Enviada com Sucesso!!!</h4>	
     </div>
-	<hr align='tr'>
     <p>Obrigado por responder a carta de recomendação solicitada a você.<br> Os dados respondidos serão analisados e irão compor o processo de seleção do candidato.</b></font></fieldset>
    	<br /><p align="right">Ass: Coordenação do PPGI/UFAM</p><br />
     
