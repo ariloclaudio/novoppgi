9c772557ec35a42c9e191429ce413fca4c29222f
thiagoleitexd <thiagoleitexd@gmail.com>
2016-03-31 13:46:33 +0000

    criacao da funcao de teste da planilha

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 use Yii;
 use app\models\Candidato;
 use app\models\Edital;
+use PHPExcel;
 use frontend\models\LoginForm;
 use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
@@ -74,11 +75,90 @@ class SiteController extends Controller
         return $this->render('opcoes',['model' => $model,
             ]);
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public function actionTesteplanilha(){
+        
+    //link: frontend/web/index.php?r=site/testeplanilha
+        
+    $objPHPExcel = new \PHPExcel();
+    
+    // Definimos o estilo da fonte
+    $objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+    
+    // Criamos as colunas
+    $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue('A1', 'Listagem de Credenciamento' )
+                ->setCellValue('B1', "Nome " )
+                ->setCellValue("C1", "Sobrenome" )
+                ->setCellValue("D1", "E-mail" );
+    
+    // Podemos configurar diferentes larguras paras as colunas como padrão
+    $objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(90);
+    $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+    $objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(30);
+    $objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(30);
+    
+    // Também podemos escolher a posição exata aonde o dado será inserido (coluna, linha, dado);
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 2, "Fulano");
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 2, " da Silva");
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3, 2, "fulano@exemplo.com.br");
+    
+    // Exemplo inserindo uma segunda linha, note a diferença no segundo parâmetro
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 3, "Beltrano");
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 3, " da Silva Sauro");
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3, 3, "beltrando@exemplo.com.br");
+    
+    // Podemos renomear o nome das planilha atual, lembrando que um único arquivo pode ter várias planilhas
+    $objPHPExcel->getActiveSheet()->setTitle('Credenciamento para o Evento');
+    
+    // Acessamos o 'Writer' para poder salvar o arquivo
+    $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+    
+    // Salva diretamente no output, poderíamos mudar arqui para um nome de arquivo em um diretório ,caso não quisessemos jogar na tela
+    $objWriter->save("arquivo.xls");
+    
+    echo "ok";
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     public function actionCadastroppgi(){
         /*if(Yii::$app->session->get('candidato') !== null)
         $this->redirect(['candidato/passo1']);*/
-        
+    
         $this->layout = '@app/views/layouts/main-login.php';
         
         $model = new Candidato();  
